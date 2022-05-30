05dda199f690f8796caf16c52328da7c290345ae
Ashutosh Kumar <akavakon72@gmail.com>
2022-05-31 01:30:41 +0530

    changes for refresh token

---
+++ src/main/java/com/ashutosh/vaultconfiguration/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.ashutosh.vaultconfiguration.controller;
+
+import com.ashutosh.vaultconfiguration.configuration.MyConfiguration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class HelloController {
+    
+    @Autowired
+    MyConfiguration myConfiguration;
+
+    @GetMapping("hello")
+    public String getHello(){
+        return myConfiguration.getUsername();
+    }
+
+}
